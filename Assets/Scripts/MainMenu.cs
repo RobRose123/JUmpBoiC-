0b29d34fbaafb04f3a1b880c9e9ae2e3f5aaba7c
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-12-02 12:14:32 +0000

    Options Menu Resolution Complete

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
     public GameObject optionsMenuHolder;
+
+    public UnityEngine.UI.Slider[] volumeSliders;
+    public UnityEngine.UI.Toggle[] resolutionToggles;
+    public int[] screenWidths;
+    int activeScreenResIndex;
+
+
+
+    public void Start()
+    {
+        activeScreenResIndex = PlayerPrefs.GetInt("screen res index");
+        bool isFullscreen = (PlayerPrefs.GetInt("fullScreen") == 1) ? true:false;
+    }
+
     public void LoadGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -30,12 +45,35 @@ public class MainMenu : MonoBehaviour
 
     public void SetScreenResolution(int i)
     {
-
+        if(resolutionToggles [i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+        }
     }
 
-    public void SetFullScreen(bool isFullsscreen)
+    public void SetFullScreen(bool isFullscreen)
     {
+    for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles [i].interactable = !isFullscreen;
+        }
 
+        if(isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution (maxResolution.width, maxResolution.height, true);
+            PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            SetScreenResolution (activeScreenResIndex);
+        }
+        PlayerPrefs.SetInt("fullscreen", ((isFullscreen) ? 1:0));
+        PlayerPrefs.Save();
     }
 
     public void SetMasterVolume(float value)
